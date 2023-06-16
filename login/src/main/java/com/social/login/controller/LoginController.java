30027b7f6bd80fc92745442fb54ea785732c702b
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-16 13:27:11 +0300

    In Login: Refactor health endpoint to GET "/health"

--- login/src/main/java/com/social/login/controller/LoginController.java
+++ login/src/main/java/com/social/login/controller/LoginController.java
@@ -1,14 +1,12 @@
 package com.social.login.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class LoginController {
 
-    @GetMapping
+    @GetMapping("/health")
     public String health() {
         return "Login service is HEALTHY.";
     }
