49de5f012975fabf6307eadd177076260d5eb7fd
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-16 13:14:00 +0300

    Create Login service

---
+++ login/src/main/java/com/social/login/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.social.login.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class LoginController {
+
+    @GetMapping
+    public String health() {
+        return "Login service is HEALTHY.";
+    }
+}
