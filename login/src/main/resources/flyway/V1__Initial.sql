bbc097fd1287132a3316427afc8ec5a363954ce5
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-18 11:36:36 +0300

    In Login: Change the log column from VARCHAR to DATE and update the imported data in the sql script

--- login/src/main/resources/flyway/V1__Initial.sql
+++ login/src/main/resources/flyway/V1__Initial.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `logs`
 (
     `id`       INT PRIMARY KEY AUTO_INCREMENT,
     `user_id`  INT                NOT NULL,
-    `log`      VARCHAR(20)        NOT NULL,
+    `log`      DATE               NOT NULL,
      CONSTRAINT `pk_logs_users`
             FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
@@ -18,7 +18,7 @@ INSERT INTO users (id, email, password) VALUES (1, 'user@user.com', 'user');
 INSERT INTO users (id, email, password) VALUES (2, 'georgi@georgi.com', 'georgi');
 INSERT INTO users (id, email, password) VALUES (3, 'stoyan@stoyan.com', 'stoyan');
 
-INSERT INTO logs (id, user_id, log) VALUES (1, '1', '2020-10-01');
-INSERT INTO logs (id, user_id, log) VALUES (2, '2', '2020-10-01');
-INSERT INTO logs (id, user_id, log) VALUES (3, '3', '2020-10-01');
-INSERT INTO logs (id, user_id, log) VALUES (4, '1', '2020-10-01');
+INSERT INTO logs (id, user_id, log) VALUES (1, '1', '2023-06-18');
+INSERT INTO logs (id, user_id, log) VALUES (2, '2', '2023-06-17');
+INSERT INTO logs (id, user_id, log) VALUES (3, '3', '2023-06-15');
+INSERT INTO logs (id, user_id, log) VALUES (4, '1', '2023-05-10');
