16af66617931132cfeee807468749bb0aac6a4dc
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-19 14:52:16 +0300

    In Profile: Implement @PostMapping("/profile") which accepts queries fomr Login Service

--- profile/src/main/java/com/social/profile/controller/ProfileController.java
+++ profile/src/main/java/com/social/profile/controller/ProfileController.java
@@ -1,14 +1,20 @@
 package com.social.profile.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class ProfileController {
 
-    @GetMapping
+    @PostMapping("/profile")
+    public String profile(@RequestParam("identity") String identity) {
+        return "In Profile Service";
+    }
+
+    @GetMapping("/health")
     public String health() {
         return "Profile service is HEALTHY.";
     }
