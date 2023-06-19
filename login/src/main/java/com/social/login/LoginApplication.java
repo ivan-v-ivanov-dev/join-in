80454a88a0f4553984ddfc5d9989ba2b4336115c
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-19 14:42:28 +0300

    In Login: EnableFeignClients and implement ProfileClient with @PostMapping("/profile")

--- login/src/main/java/com/social/login/LoginApplication.java
+++ login/src/main/java/com/social/login/LoginApplication.java
@@ -2,12 +2,14 @@ package com.social.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LoginApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
 
 }
