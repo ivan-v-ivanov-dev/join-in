b645f37ec27658f7cd83ec2738a04183b1e868aa
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-22 08:53:14 +0300

    In Profile: Exclude DataSourceAutoConfiguration

--- profile/src/main/java/com/social/profile/ProfileApplication.java
+++ profile/src/main/java/com/social/profile/ProfileApplication.java
@@ -2,12 +2,13 @@ package com.social.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ProfileApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProfileApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileApplication.class, args);
+    }
 
 }
