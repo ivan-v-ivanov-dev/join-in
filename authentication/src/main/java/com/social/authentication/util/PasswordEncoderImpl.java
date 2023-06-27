da3afc1d8aae1e24056449b53485214cefc0dc02
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-27 08:51:27 +0300

    In Authentication: Implement PasswordEncoder.   - Implement PasswordEncoder interface   - Rename PasswordEncoderImpl class   - Implement String encode(String password) in PasswordEncoderImpl

--- authentication/src/main/java/com/social/authentication/util/PasswordEncoderImpl.java
+++ authentication/src/main/java/com/social/authentication/util/PasswordEncoderImpl.java
@@ -1,18 +1,23 @@
 package com.social.authentication.util;
 
+import com.social.authentication.util.contracts.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PasswordEncoder {
+public class PasswordEncoderImpl implements PasswordEncoder {
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public PasswordEncoder() {
+    public PasswordEncoderImpl() {
         this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
     }
 
     public boolean areEqual(String queryPassword, String databasePassword) {
         return bCryptPasswordEncoder.matches(queryPassword, databasePassword);
+    }
+
+    public String encode(String password) {
+        return bCryptPasswordEncoder.encode(password);
     }
 }
