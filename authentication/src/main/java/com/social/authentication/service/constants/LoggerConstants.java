f98fd5ee384067d22dab36d6ca1eed843c984e86
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-27 16:49:31 +0300

    In Authentication: Remove typos in LoggerConstants

--- authentication/src/main/java/com/social/authentication/service/constants/LoggerConstants.java
+++ authentication/src/main/java/com/social/authentication/service/constants/LoggerConstants.java
@@ -4,9 +4,9 @@ public class LoggerConstants {
 
     public static final String FAILED_TO_AUTHENTICATE_WITH_CREDENTIALS = "Failed to Authenticate with input credentials.";
     public static final String USER_LOGGED_TEMPLATE = "User %s logged.";
-    public static final String USER_LOGIN_SAVED_IN_DATABASE_TEMPLATE = "User %s login saved in database";
-    public static final String REDIRECTING_TO_PROFILE_SERVICE = "Redirecting to Profile servis";
-    public static final String CAN_NOT_REDIRECT_TO_PROFILE_SERVICE_TEMPLATE = "Can't redirect to profile service:: %s";
+    public static final String USER_LOGIN_SAVED_IN_DATABASE_TEMPLATE = "User %s login saved in database.";
+    public static final String REDIRECTING_TO_PROFILE_SERVICE = "Redirecting to Profile service.";
+    public static final String CAN_NOT_REDIRECT_TO_PROFILE_SERVICE_TEMPLATE = "Can't redirect to profile service:: %s.";
 
     private LoggerConstants() {
     }
