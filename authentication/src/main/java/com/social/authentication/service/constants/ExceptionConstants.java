a063b4c696af4bf9f523f12cc40380117a92ac2f
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-27 17:48:21 +0300

    In Authentication: Implement ExceptionConstants and ModelEntityConstants classes with private constructors

---
+++ authentication/src/main/java/com/social/authentication/service/constants/ExceptionConstants.java
@@ -0,0 +1,11 @@
+package com.social.authentication.service.constants;
+
+public class ExceptionConstants {
+
+    public static final String USER_EMAIL_IS_ALREADY_IN_USE = "User email is already in use.";
+    public static final String BAD_CREDENTIALS = "Bad credentials";
+    public static final String PROFILE_PAGE_CAN_NOT_BE_LOADED = "Profile page can't be loaded.";
+
+    private ExceptionConstants() {
+    }
+}
