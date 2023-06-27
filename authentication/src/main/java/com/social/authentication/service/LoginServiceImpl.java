a37f831475e1b3518cecff6ded573a1ea364b29d
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-27 10:00:17 +0300

    In Authentication: Enable Slf4j logging:  - Implement logger messages in LoginServiceImpl  - Implement LoggerConstants class with private constructor

--- authentication/src/main/java/com/social/authentication/service/LoginServiceImpl.java
+++ authentication/src/main/java/com/social/authentication/service/LoginServiceImpl.java
@@ -6,13 +6,18 @@ import com.social.authentication.repository.LogRepository;
 import com.social.authentication.repository.UserRepository;
 import com.social.authentication.service.contract.LoginService;
 import com.social.authentication.service.feign.ProfileClient;
-import com.social.authentication.util.PasswordEncoder;
+import com.social.authentication.util.PasswordEncoderImpl;
+import com.social.authentication.util.contracts.PasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
+import static com.social.authentication.service.constants.LoggerConstants.*;
+
 @Service
+@Slf4j
 public class LoginServiceImpl implements LoginService {
 
     private final LogRepository logRepository;
@@ -22,7 +27,7 @@ public class LoginServiceImpl implements LoginService {
 
     public LoginServiceImpl(LogRepository logRepository,
                             UserRepository userRepository,
-                            PasswordEncoder passwordEncoder,
+                            PasswordEncoderImpl passwordEncoder,
                             ProfileClient profileClient) {
         this.logRepository = logRepository;
         this.userRepository = userRepository;
@@ -35,11 +40,17 @@ public class LoginServiceImpl implements LoginService {
         User loggedUser = userRepository.findByEmail(email);
 
         if (loggedUser != null && passwordEncoder.areEqual(password, loggedUser.getPassword())) {
+            log.info(String.format(USER_LOGGED_TEMPLATE, email));
+
             Log loggedUserLog = Log.builder().id(UUID.randomUUID().toString()).user(loggedUser).logDate(LocalDate.now()).build();
             logRepository.save(loggedUserLog);
+            log.info(String.format(USER_LOGIN_SAVED_IN_DATABASE_TEMPLATE, email));
+
+            log.info(REDIRECTING_TO_PROFILE_SERVICE);
             profileClient.profile(loggedUser.getIdentity());
         }
 
+        log.error(FAILED_TO_AUTHENTICATE_WITH_CREDENTIALS);
         throw new IllegalArgumentException();
     }
 }
