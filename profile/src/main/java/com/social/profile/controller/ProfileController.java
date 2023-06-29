c937637ea39a1018326d3aaab62d41f0082527f7
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-29 18:27:09 +0300

    In Profile: Implement @PostMapping("/") MVC controller which generates the profile page

--- profile/src/main/java/com/social/profile/controller/ProfileController.java
+++ profile/src/main/java/com/social/profile/controller/ProfileController.java
@@ -1,14 +1,14 @@
 package com.social.profile.controller;
 
-
 import com.social.profile.model.Profile;
 import com.social.profile.service.contrancts.ProfileService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ProfileController {
 
     private final ProfileService profileService;
@@ -17,13 +17,15 @@ public class ProfileController {
         this.profileService = profileService;
     }
 
+    @PostMapping("/")
+    public String profile(@RequestParam("identity") String identity, Model model) {
+        Profile byIdentity = this.profileService.findByIdentity(identity);
+        model.addAttribute("profile", this.profileService.findByIdentity(identity));
+        return "profile";
+    }
+
     @GetMapping("/health")
     public String health() {
         return "Profile service is HEALTHY.";
-    }
-
-    @PostMapping("/")
-    public Profile profile(@RequestParam("identity") String identity) {
-        return this.profileService.findByIdentity(identity);
     }
 }
