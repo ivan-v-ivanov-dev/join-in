2c49f98cbd197d155fbb5c45576e0e9b1689d5a1
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-29 16:51:39 +0300

    In Profile: Exclude automatic configurations (JPA, Hibernate, Mongo) in Spring

--- profile/src/main/java/com/social/profile/ProfileApplication.java
+++ profile/src/main/java/com/social/profile/ProfileApplication.java
@@ -3,8 +3,15 @@ package com.social.profile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        MongoAutoConfiguration.class})
 public class ProfileApplication {
 
     public static void main(String[] args) {
