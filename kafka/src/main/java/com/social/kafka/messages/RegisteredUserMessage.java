c2ecc56897c73fcbbeb2c81cd758e4b314e53254
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-03 10:10:50 +0300

    In Kafka: Implement KafkaMessage Entities for message exchange in Kafka

---
+++ kafka/src/main/java/com/social/kafka/messages/RegisteredUserMessage.java
@@ -0,0 +1,17 @@
+package com.social.kafka.messages;
+
+import com.social.kafka.messages.contract.KafkaMessage;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class RegisteredUserMessage implements KafkaMessage {
+
+    private String identity;
+    private String email;
+}
---
+++ kafka/src/main/java/com/social/kafka/messages/contract/KafkaMessage.java
@@ -0,0 +1,6 @@
+package com.social.kafka.messages.contract;
+
+import java.io.Serializable;
+
+public interface KafkaMessage extends Serializable {
+}
