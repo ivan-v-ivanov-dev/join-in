a384f2295060283322a233278dc5df830c14f41d
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-03 13:58:11 +0300

    In Profile: Enable logging in listener:  - add logging messages in the listener  - add group id application variable  - use application variables in listener

--- profile/src/main/java/com/social/profile/service/RegisteredUserListener.java
+++ profile/src/main/java/com/social/profile/service/RegisteredUserListener.java
@@ -3,22 +3,32 @@ package com.social.profile.service;
 import com.social.kafka.messages.RegisteredUserMessage;
 import com.social.kafka.messages.contract.KafkaMessage;
 import com.social.profile.model.Profile;
-import com.social.profile.service.contrancts.ProfileService;
+import com.social.profile.service.contracts.ProfileService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import static com.social.profile.service.constants.LoggerConstants.KAFKA_MESSAGE_FROM_AUTHENTICATION_SERVICE_IN_TOPIC_FOR_NEW_REGISTERED_USER_RECEIVED_TEMPLATE;
+import static com.social.profile.service.constants.LoggerConstants.SAVE_THE_USER_IN_DATABASE_TEMPLATE;
+
 @Service
+@Slf4j
 public class RegisteredUserListener {
 
     private final ProfileService profileService;
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
 
     public RegisteredUserListener(ProfileService profileService) {
         this.profileService = profileService;
     }
 
-    @KafkaListener(topics = "registered-user-profile-topic", groupId = "group_id")
+    @KafkaListener(topics = "#{'${spring.kafka.topic.name}'}", groupId = "#{'${spring.kafka.group.id}'}")
     public void shopListener(KafkaMessage kafkaMessage) {
         RegisteredUserMessage registeredUserMessage = (RegisteredUserMessage) kafkaMessage;
+        log.info(String.format(KAFKA_MESSAGE_FROM_AUTHENTICATION_SERVICE_IN_TOPIC_FOR_NEW_REGISTERED_USER_RECEIVED_TEMPLATE,
+                topicName, registeredUserMessage.getIdentity()));
 
         Profile profile = Profile.builder()
                 .identity(registeredUserMessage.getIdentity())
@@ -28,6 +38,7 @@ public class RegisteredUserListener {
                 .joined(registeredUserMessage.getJoined())
                 .build();
 
+        log.info(String.format(SAVE_THE_USER_IN_DATABASE_TEMPLATE, profile.getIdentity()));
         profileService.save(profile);
     }
 }
---
+++ profile/src/main/java/com/social/profile/service/constants/LoggerConstants.java
@@ -0,0 +1,12 @@
+package com.social.profile.service.constants;
+
+public class LoggerConstants {
+
+    public static final String KAFKA_MESSAGE_FROM_AUTHENTICATION_SERVICE_IN_TOPIC_FOR_NEW_REGISTERED_USER_RECEIVED_TEMPLATE =
+            "Kafka message from Authentication service in topic %s for new registered user %s received";
+    public static final String SAVE_THE_USER_IN_DATABASE_TEMPLATE =
+            "Save user %s in database";
+
+    private LoggerConstants() {
+    }
+}
