a90fa6c2b92f5acdb69becd5fa173191551d34cf
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-03 12:21:23 +0300

    In Kafka: Add fistName, lastName and joined field in RegisteredUserMessage

--- kafka/src/main/java/com/social/kafka/messages/RegisteredUserMessage.java
+++ kafka/src/main/java/com/social/kafka/messages/RegisteredUserMessage.java
@@ -4,6 +4,8 @@ import com.social.kafka.messages.contract.KafkaMessage;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,5 +15,13 @@ import org.springframework.stereotype.Component;
 public class RegisteredUserMessage implements KafkaMessage {
 
     private String identity;
+
+    private String firstName;
+
+    private String lastName;
+
     private String email;
+
+    private LocalDate joined;
+
 }
