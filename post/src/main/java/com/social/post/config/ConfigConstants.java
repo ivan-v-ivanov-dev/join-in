542891367cb6b974d556b811a8746f79eb8b00aa
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-04 12:51:47 +0300

    In Post: Add RedisConfig with application variables and logger messages

---
+++ post/src/main/java/com/social/post/config/ConfigConstants.java
@@ -0,0 +1,17 @@
+package com.social.post.config;
+
+public class ConfigConstants {
+
+    // Redis Configuration Logger Constants
+    public static final String REDIS_CONFIGURATION = "Redis Configuration :: ";
+    public static final String REDIS_CONFIGURATION_REDIS_STANDALONE_CONFIGURATION_CREATED = REDIS_CONFIGURATION +
+            "RedisStandaloneConfiguration Created";
+    public static final String REDIS_CONFIGURATION_JEDIS_CONNECTION_FACTORY_CREATED = REDIS_CONFIGURATION +
+            "JedisConnectionFactory Created";
+    public static final String REDIS_CONFIGURATION_OBJECT_MAPPER_CREATED = REDIS_CONFIGURATION +
+            "ObjectMapper Created";
+    public static final String REDIS_CONFIGURATION_JACKSON_2_JSON_REDIS_SERIALIZER_CREATED = REDIS_CONFIGURATION +
+            "Jackson2JsonRedisSerializer Created";
+    public static final String REDIS_CONFIGURATION_REDIS_TEMPLATE_CREATED = REDIS_CONFIGURATION +
+            "RedisTemplate Created";
+}
