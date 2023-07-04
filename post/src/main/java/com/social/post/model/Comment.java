47b22adaeed91213784ef9ae4c4e0dd9b31082cd
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-04 12:52:36 +0300

    In Post: Implement Comment POJO for redis

---
+++ post/src/main/java/com/social/post/model/Comment.java
@@ -0,0 +1,14 @@
+package com.social.post.model;
+
+import java.time.LocalDate;
+
+public class Comment {
+
+    private String authorIdentity;
+
+    private String commentIdentity;
+
+    private String content;
+
+    private LocalDate postDate;
+}
