60a4c84cec7bc4696d1c411028f3ae387d8ec735
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-06 12:56:27 +0300

    In Kafka: Refactor field in RegisteredUserMessage

--- kafka/src/main/java/com/social/kafka/messages/RegisteredUserMessage.java
+++ kafka/src/main/java/com/social/kafka/messages/RegisteredUserMessage.java
@@ -4,8 +4,6 @@ import com.social.kafka.messages.contract.KafkaMessage;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-
 @Component
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,12 +14,8 @@ public class RegisteredUserMessage implements KafkaMessage {
 
     private String identity;
 
-    private String firstName;
-
-    private String lastName;
-
     private String email;
 
-    private LocalDate joined;
+    private String password;
 
 }
