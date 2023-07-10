fcfd6e62961fea7a7c7d7950773ce2243dc2b944
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-10 13:36:16 +0300

    In Post: Add sample dummy comment data for testing purposes

--- post/src/main/resources/mongo/init.js
+++ post/src/main/resources/mongo/init.js
@@ -6,18 +6,19 @@ use ("storage");
 db.createCollection("posts");
 console.log('*** Collection created ***')
 
-db.posts.insertMany([
+db.posts.insertMany(
+[
     {
-        authorIdentity: "authorIdentity",
+        authorIdentity: "1567b3cb857f132473d4907fd3fee2f96e661308826522e823f5f2e6e20b6032",
         postIdentity: "postIdentity11",
         content: "content",
-        postDate: null
+        postDate: "2021-01-23"
     },
     {
-        authorIdentity: "authorIdentity",
+        authorIdentity: "1567b3cb857f132473d4907fd3fee2f96e661308826522e823f5f2e6e20b6032",
         postIdentity: "postIdentity22",
         content: "content",
-        postDate: null
+        postDate: "2023-07-09"
     }
 ]);
 console.log('*** Data Imported ***')
