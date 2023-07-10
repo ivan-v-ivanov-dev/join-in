5f9a9ae756691024f0ccc91d50effcfbd637c2cb
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-10 17:42:15 +0300

    In Kafka: Add DeletePostMessage

---
+++ kafka/src/main/java/com/social/kafka/messages/DeletePostMessage.java
@@ -0,0 +1,14 @@
+package com.social.kafka.messages;
+
+import com.social.kafka.messages.contract.KafkaMessage;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class DeletePostMessage implements KafkaMessage {
+
+    private String postIdentity;
+}
