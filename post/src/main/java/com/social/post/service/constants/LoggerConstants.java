bf8fd10eeaf86a208864b275e36d687c1716f2a0
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-10 17:49:16 +0300

    In Post: Add message constant for the received delete message in DeletePostListener

--- post/src/main/java/com/social/post/service/constants/LoggerConstants.java
+++ post/src/main/java/com/social/post/service/constants/LoggerConstants.java
@@ -9,6 +9,9 @@ public class LoggerConstants {
     public static final String NEW_COMMENT_PUBLISHED_MESSAGE_RECEIVED_FROM_PROFILE_SERVICE_TEMPLATE =
             "Kafka Message for new comment posted by User %s received from Profile service (Kafka topic: %s)";
 
+    public static final String NEW_DELETE_POST_MESSAGE_RECEIVED_FROM_PROFILE_SERVICE_TEMPLATE =
+            "Kafka Message for post deleting Post %s received from Profile service (Kafka topic: %s)";
+
     // Post create
     public static final String NEW_POST_SAVED_IN_DATABASE_TEMPLATE =
             "New Post saved in database (User identity %s, Post identity: %s)";
