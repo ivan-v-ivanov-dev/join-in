83d11cfcaa3f4fb486af99235e1ceb214c3253ae
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-14 13:22:36 +0300

    In Graph: Add Queries class to store string query constants

---
+++ graph/src/main/java/com/social/graph/repository/queries/Queries.java
@@ -0,0 +1,10 @@
+package com.social.graph.repository.queries;
+
+public class Queries {
+
+    public static final String FIND_THE_FRIENDS_FOR_A_PROFILE_BY_PROFILE_IDENTITY_TEMPLATE =
+            "MATCH (p:Profile)-[:FRIEND]->(friend:Profile) WHERE p.identity = $identity RETURN friend";
+
+    private Queries() {
+    }
+}
