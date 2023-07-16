9efe4e04414a6de66a631c85a5dfff7baf9a1202
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-16 20:29:33 +0300

    In Reaction: Refactor query constants for count of post likes, dislikes and stars

--- reaction/src/main/java/com/social/reaction/repository/queries/Queries.java
+++ reaction/src/main/java/com/social/reaction/repository/queries/Queries.java
@@ -3,13 +3,13 @@ package com.social.reaction.repository.queries;
 public class Queries {
 
     public static final String FIND_HOW_MANY_PROFILES_LIKE_THE_POST_TEMPLATE =
-            "MATCH (p:Profile)-[:LIKE]->(:Post {identity: $postIdentity}) RETURN count(p)";
+            "MATCH (p:Profile)-[:LIKE]->(post:Post) WHERE post.identity = $postIdentity RETURN COUNT(DISTINCT p)";
 
     public static final String FIND_HOW_MANY_PROFILES_DISLIKE_THE_POST_TEMPLATE =
-            "MATCH (p:Profile)-[:DISLIKE]->(:Post {identity: $postIdentity}) RETURN count(p)";
+            "MATCH (p:Profile)-[:DISLIKE]->(post:Post) WHERE post.identity = $postIdentity RETURN COUNT(DISTINCT p)";
 
     public static final String FIND_HOW_MANY_PROFILES_STAR_THE_POST_TEMPLATE =
-            "MATCH (p:Profile)-[:STAR]->(:Post {identity: $postIdentity}) RETURN count(p)";
+            "MATCH (p:Profile)-[:STAR]->(post:Post) WHERE post.identity = $postIdentity RETURN COUNT(DISTINCT p)";
 
     private Queries() {
     }
