722b64f10902d193dd5ad93c2c2c0238cead608b
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-16 18:44:52 +0300

    In Post: Add CollectionTemplateConstant constant which generates the Collection template with prefix c_ and userIdentity

---
+++ post/src/main/java/com/social/post/service/constants/CollectionTemplateConstant.java
@@ -0,0 +1,9 @@
+package com.social.post.service.constants;
+
+public class CollectionTemplateConstant {
+
+    public static final String COLLECTION_TEMPLATE = "c_%s";
+
+    private CollectionTemplateConstant() {
+    }
+}
