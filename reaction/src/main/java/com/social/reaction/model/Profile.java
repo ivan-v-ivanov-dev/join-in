5c3985dcb18895f2ba3fa1bc860cba68e08e95b0
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-16 14:17:42 +0300

    In Reaction: Add Profile node

---
+++ reaction/src/main/java/com/social/reaction/model/Profile.java
@@ -0,0 +1,22 @@
+package com.social.reaction.model;
+
+import lombok.*;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+import java.io.Serializable;
+
+@Node(labels = "Profile")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Profile implements Serializable {
+
+    @Id
+    private String identity;
+
+    private String email;
+
+}
