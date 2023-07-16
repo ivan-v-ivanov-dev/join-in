b834c7218ff972667c7ab68c8aee0bc6e095ff3b
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-16 18:46:51 +0300

    In Profile: Refactor: Remove typo from url name for  ReactionClient

--- profile/src/main/java/com/social/profile/service/feign/ReactionClient.java
+++ profile/src/main/java/com/social/profile/service/feign/ReactionClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "${reaction.service.feign.client.name}", url = "${reactionv.service.url}")
+@FeignClient(name = "${reaction.service.feign.client.name}", url = "${reaction.service.url}")
 public interface ReactionClient {
 
     @PostMapping("${reaction.find.likes.count}")
