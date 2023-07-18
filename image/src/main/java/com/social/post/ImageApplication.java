2e23b5b138b4081d3a61a97055b50bfa23e314b6
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-18 16:31:19 +0300

    Create Image service running on port 8085

---
+++ image/src/main/java/com/social/post/ImageApplication.java
@@ -0,0 +1,21 @@
+package com.social.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        MongoAutoConfiguration.class})
+public class ImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageApplication.class, args);
+    }
+
+}
