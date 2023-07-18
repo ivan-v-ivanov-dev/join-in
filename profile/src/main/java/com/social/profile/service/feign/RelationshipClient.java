de4d2fa78ad72d0d056b57172e0a6aa25095390a
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-18 20:40:00 +0300

    In Profile: Add @PostMapping("${relationship.find.friends.for.user}") to RelationshipClient

--- profile/src/main/java/com/social/profile/service/feign/RelationshipClient.java
+++ profile/src/main/java/com/social/profile/service/feign/RelationshipClient.java
@@ -4,9 +4,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @FeignClient(name = "${relationship.service.feign.client.name}", url = "${relationship.service.url}")
 public interface RelationshipClient {
 
+    @PostMapping("${relationship.find.friends.for.user}")
+    List<String> findFriends(@RequestParam("identity") String identity);
+
     @PostMapping("${relationship.find.friends.count}")
-    int findFriendCountByProfileIdentity(@RequestParam("identity") String identity);
+    int findFriendCount(@RequestParam("identity") String identity);
 }
