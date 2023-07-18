4574c191ea547a71f119e962e5e13c4c1935c31a
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-18 19:04:10 +0300

    In Image: Add ALBUM_IMAGES_FIELD constant

--- image/src/main/java/com/social/post/repository/constants/Constants.java
+++ image/src/main/java/com/social/post/repository/constants/Constants.java
@@ -3,7 +3,10 @@ package com.social.post.repository.constants;
 public class Constants {
 
     public static final String PROFILE_IMAGE_FIELD = "profileImage";
+
     public static final String BACKGROUND_IMAGE_FIELD = "backgroundImage";
+
+    public static final String ALBUM_IMAGES_FIELD = "albumImages";
 
     private Constants(){}
 }
