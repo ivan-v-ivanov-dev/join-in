6308dd10e9c2f0c9d5e6dd58af6e3394a1d50375
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-19 18:07:07 +0300

    In Relationship: Add logging when saving new user in database

--- relationship/src/main/java/com/social/relationship/service/ProfileServiceImpl.java
+++ relationship/src/main/java/com/social/relationship/service/ProfileServiceImpl.java
@@ -3,11 +3,15 @@ package com.social.relationship.service;
 import com.social.relationship.model.Profile;
 import com.social.relationship.repository.ProfileRepository;
 import com.social.relationship.service.contracts.ProfileService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.social.relationship.service.constants.LoggerConstants.NEW_USER_SAVED_IN_DATABASE_TEMPLATE;
+
 @Service
+@Slf4j
 public class ProfileServiceImpl implements ProfileService {
 
     private final ProfileRepository profileRepository;
@@ -24,6 +28,7 @@ public class ProfileServiceImpl implements ProfileService {
     @Override
     public void save(Profile profile) {
         profileRepository.save(profile);
+        log.info(String.format(NEW_USER_SAVED_IN_DATABASE_TEMPLATE, profile.getIdentity()));
     }
 
     @Override
