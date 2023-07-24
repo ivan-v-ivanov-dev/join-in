04b1910dc0f12dc5405402a0ba98d4f097665c7c
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-24 17:15:13 +0300

    In Kafka: NewPostNodeMessage added for sending messages to Reaction service when post is published

---
+++ kafka/src/main/java/com/social/kafka/messages/NewPostNodeMessage.java
@@ -0,0 +1,14 @@
+package com.social.kafka.messages;
+
+import com.social.kafka.messages.contract.KafkaMessage;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class NewPostNodeMessage implements KafkaMessage {
+
+    private String postIdentity;
+}
