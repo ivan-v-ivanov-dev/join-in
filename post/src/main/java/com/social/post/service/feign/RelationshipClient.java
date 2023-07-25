537dad19f769fd4c99c08876cb0dcb464b8068ce
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-25 13:12:01 +0300

    In Post: Relationship Client retrieves Set<String> instead List<String>

--- post/src/main/java/com/social/post/service/feign/RelationshipClient.java
+++ post/src/main/java/com/social/post/service/feign/RelationshipClient.java
@@ -4,11 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.util.Set;
 
 @FeignClient(name = "${relationship.service.feign.client.name}", url = "${relationship.service.url}")
 public interface RelationshipClient {
 
     @PostMapping("${relationship.find.profile.friends}")
-    List<String> findFriends(@RequestParam("identity") String identity);
+    Set<String> findFriends(@RequestParam("identity") String identity);
 }
