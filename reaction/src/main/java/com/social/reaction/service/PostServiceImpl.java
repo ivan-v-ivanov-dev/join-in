651f0ad71be0de0eff12bf32816793a56acb6b5e
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-25 14:32:27 +0300

    In Reaction: Add logging when saving new Post node

--- reaction/src/main/java/com/social/reaction/service/PostServiceImpl.java
+++ reaction/src/main/java/com/social/reaction/service/PostServiceImpl.java
@@ -3,9 +3,13 @@ package com.social.reaction.service;
 import com.social.reaction.model.Post;
 import com.social.reaction.repository.PostRepository;
 import com.social.reaction.service.contracts.PostService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import static com.social.reaction.service.constants.LoggerConstants.SAVE_POST_IN_DATABASE_TEMPLATE;
+
 @Service
+@Slf4j
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
@@ -17,5 +21,6 @@ public class PostServiceImpl implements PostService {
     @Override
     public void save(Post post) {
         postRepository.save(post);
+        log.info(String.format(SAVE_POST_IN_DATABASE_TEMPLATE, post.getIdentity()));
     }
 }
