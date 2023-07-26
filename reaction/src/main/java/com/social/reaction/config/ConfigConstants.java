313102e70437a609b871cd199bc69c1356c0eaee
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-26 15:25:58 +0300

    In Reaction: Add LikePostNotificationTopic to send Kafka messages to Notification service

--- reaction/src/main/java/com/social/reaction/config/ConfigConstants.java
+++ reaction/src/main/java/com/social/reaction/config/ConfigConstants.java
@@ -18,6 +18,10 @@ public class ConfigConstants {
     public static final String CONCURRENT_KAFKA_LISTENER_CONTAINER_FACTORY_FOR_KAFKA_MESSAGING_CREATED =
             KAFKA_CONFIGURATION + "Concurrent Kafka Listener Container Factor" + FOR_KAFKA_MESSAGING_CREATED;
 
+    // Kafka Topic
+    public static final String KAFKA_TOPIC_FOR_LIKE_A_POST_CREATED_TEMPLATE =
+            "Kafka Topic :: New topic for like a post created (name:  %s)";
+
     private ConfigConstants() {
     }
 }
