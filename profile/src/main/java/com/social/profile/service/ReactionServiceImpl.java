f6461f96e1183c603c62dc38c1fe5ad873ea5fdf
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-26 15:13:30 +0300

    In Profile: Send postAuthorNames with ReactionMessage to Reaction service

--- profile/src/main/java/com/social/profile/service/ReactionServiceImpl.java
+++ profile/src/main/java/com/social/profile/service/ReactionServiceImpl.java
@@ -2,6 +2,7 @@ package com.social.profile.service;
 
 import com.social.kafka.messages.ReactionMessage;
 import com.social.kafka.messages.contract.KafkaMessage;
+import com.social.profile.repository.contract.ProfileRepository;
 import com.social.profile.service.contracts.KafkaMessageSender;
 import com.social.profile.service.contracts.ReactionService;
 import lombok.extern.slf4j.Slf4j;
@@ -9,16 +10,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import static com.social.profile.service.constants.LoggerConstants.LIKE_POST_CREATED_AND_SEND_TO_REACTION_SERVICE_TOPIC_NAME_POST_IDENTITY_TEMPLATE;
+import static com.social.profile.service.constants.ServiceConstants.AUTHOR_NAME_TEMPLATE;
 
 @Service
 @Slf4j
 public class ReactionServiceImpl implements ReactionService {
 
+    private final ProfileRepository profileRepository;
     private final KafkaMessageSender kafkaMessageSender;
     private final String likePostTopic;
 
-    public ReactionServiceImpl(KafkaMessageSender kafkaMessageSender,
+    public ReactionServiceImpl(ProfileRepository profileRepository,
+                               KafkaMessageSender kafkaMessageSender,
                                @Value("${spring.kafka.topic.name.like.post}") String likePostTopic) {
+        this.profileRepository = profileRepository;
         this.kafkaMessageSender = kafkaMessageSender;
         this.likePostTopic = likePostTopic;
     }
@@ -29,6 +34,9 @@ public class ReactionServiceImpl implements ReactionService {
                 .reactingUserIdentity(reactingUserIdentity)
                 .postIdentity(postIdentity)
                 .postAuthorIdentity(postAuthorIdentity)
+                .postAuthorNames(String.format(AUTHOR_NAME_TEMPLATE,
+                        profileRepository.findProfileFirstName(postAuthorIdentity),
+                        profileRepository.findProfileLastName(postAuthorIdentity)))
                 .build();
 
         kafkaMessageSender.send(postReactionMessage, likePostTopic);
