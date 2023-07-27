1f8693bf909a143c2a2c38f047a0bea032da640d
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-27 10:49:03 +0300

    In Post: Rename CollectionTemplateConstant to ServiceConstant

--- post/src/main/java/com/social/post/service/constants/CollectionTemplateConstant.java
+++
@@ -1,9 +0,0 @@
-package com.social.post.service.constants;
-
-public class CollectionTemplateConstant {
-
-    public static final String COLLECTION_TEMPLATE = "c_%s";
-
-    private CollectionTemplateConstant() {
-    }
-}
