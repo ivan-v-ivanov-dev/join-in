9d6ec109710aac650b14e7369afcc7f1a024a59e
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-31 17:38:52 +0300

    In Profile: Add DislikesCommentTopic

--- profile/src/main/java/com/social/profile/config/ConfigConstants.java
+++ profile/src/main/java/com/social/profile/config/ConfigConstants.java
@@ -58,6 +58,9 @@ public class ConfigConstants {
     public static final String KAFKA_TOPIC_FOR_LIKE_COMMENT_TEMPLATE =
             KAFKA_TOPIC_NEW_TOPIC_FOR + "like comment" + CREATED_TOPIC_NAME;
 
+    public static final String KAFKA_TOPIC_FOR_DISLIKE_COMMENT_TEMPLATE =
+            KAFKA_TOPIC_NEW_TOPIC_FOR + "dislike comment" + CREATED_TOPIC_NAME;
+
     private ConfigConstants() {
     }
 }
