e2dcff9dd2c377c5a393e550c7eff9315b824f30
Ivan Ivanov <ivanov.biol@gmail.com>
2023-07-31 10:20:50 +0300

    In Post: Add sample user comment for testing purposes

--- post/src/main/resources/mongo/init.js
+++ post/src/main/resources/mongo/init.js
@@ -37,13 +37,13 @@ db.c_1567b3cb857f132473d4907fd3fee2f96e661308826522e823f5f2e6e20b6032.insertMany
                 {
                     commentIdentity: "d0b214d649e2abb2259a1d8dc6c79f877dc2e3f04684f47c22674b5d63c752949e36cd588d733d047f714a2395fb310a68893d635ea00839bf849cf652c4dcdd",
                     authorIdentity: "771fda51cc819105d8f3ce802fce2a4f33d23054c5a102f19872fad2a3208551",
-                    content: "That is cool, I've done such a thing.",
+                    content: "OK, ping me in private message.",
                     postDate: "2021-01-23"
                 },
                 {
                    commentIdentity: "7f608f8b9cd3b674fa4549792a26ecef4b9ce3635ed9b5fb1643e29ff47d09f52ec6cfc0b2846cb06f605c15c7daf108c88b2ae41a4e163283980da54142b417",
                    authorIdentity: "788176f5bb7f654863c2f3aa6bfaa8b49f54d0288019e9d846f772467984b627",
-                   content: "Yes, I've done it.",
+                   content: "For the microservices you can use Kafka as message broker.",
                    postDate: "2021-01-23"
                 }
         ],
