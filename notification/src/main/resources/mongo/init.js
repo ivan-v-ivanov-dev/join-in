ae8fabe701de9836c00e3a257c172e5e270e9be9
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-01 17:17:41 +0300

    In Notification: Delete half of the notifications sample data - page can't fit them all

--- notification/src/main/resources/mongo/init.js
+++ notification/src/main/resources/mongo/init.js
@@ -10,41 +10,6 @@ console.log('*** Collection created ***')
 db.c_1567b3cb857f132473d4907fd3fee2f96e661308826522e823f5f2e6e20b6032.insertMany([
     {
         authorIdentity: "771fda51cc819105d8f3ce802fce2a4f33d23054c5a102f19872fad2a3208551",
-        postIdentity: "79cbd7ac103374bc6f9885eed885fec16c8c51d6d2fae9529361323065ab1c0b7e2af2f8e006622a7e321d4e1bf9e38ad0187f75ca35c0b9d8ca4b9e705484d4",
-        notification: "Georgi Tihomirov wrote a new comment",
-        date: "2021-01-23",
-        seen: false
-    },
-    {
-        authorIdentity: "788176f5bb7f654863c2f3aa6bfaa8b49f54d0288019e9d846f772467984b627",
-        postIdentity: "79cbd7ac103374bc6f9885eed885fec16c8c51d6d2fae9529361323065ab1c0b7e2af2f8e006622a7e321d4e1bf9e38ad0187f75ca35c0b9d8ca4b9e705484d4",
-        notification: "Stoyan Dimitrov wrote a new comment",
-        date: "2021-01-23",
-        seen: false
-    },
-    {
-        authorIdentity: "771fda51cc819105d8f3ce802fce2a4f33d23054c5a102f19872fad2a3208551",
-        postIdentity: "79cbd7ac103374bc6f9885eed885fec16c8c51d6d2fae9529361323065ab1c0b7e2af2f8e006622a7e321d4e1bf9e38ad0187f75ca35c0b9d8ca4b9e705484d4",
-        notification: "Georgi Tihomirov likes a post",
-        date: "2021-01-23",
-        seen: false
-    },
-    {
-        authorIdentity: "788176f5bb7f654863c2f3aa6bfaa8b49f54d0288019e9d846f772467984b627",
-        postIdentity: "79cbd7ac103374bc6f9885eed885fec16c8c51d6d2fae9529361323065ab1c0b7e2af2f8e006622a7e321d4e1bf9e38ad0187f75ca35c0b9d8ca4b9e705484d4",
-        notification: "Stoyan Dimitrov likes a post",
-        date: "2022-10-23",
-        seen: false
-    },
-    {
-        authorIdentity: "d52ae056d5830fe0a58a7da85465d6561bb0b9aba01706f010c585810b8fd3c8",
-        postIdentity: "79cbd7ac103374bc6f9885eed885fec16c8c51d6d2fae9529361323065ab1c0b7e2af2f8e006622a7e321d4e1bf9e38ad0187f75ca35c0b9d8ca4b9e705484d4",
-        notification: "Viktoriya Ivanova stared a post",
-        date: "2022-01-23",
-        seen: false
-    },
-    {
-        authorIdentity: "771fda51cc819105d8f3ce802fce2a4f33d23054c5a102f19872fad2a3208551",
         postIdentity: "cc66a46cad4b32f69a840a0aa29aeb30d6c014c1f09cdd9de446d7da4fcce1c5d97303c201d77a21b8f35ec67c081dd5dd01fe4637782aff360b0b968ad07e97",
         notification: "Georgi Tihomirov wrote a new comment",
         date: "2023-07-23",
