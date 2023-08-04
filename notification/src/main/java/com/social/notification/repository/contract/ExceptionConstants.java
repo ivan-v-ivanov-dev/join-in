a5711e67c32b273b8eed265231c9a5f018208ab1
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-04 10:27:23 +0300

    In Notification: Error handling ImageClient FeignException

---
+++ notification/src/main/java/com/social/notification/repository/contract/ExceptionConstants.java
@@ -0,0 +1,10 @@
+package com.social.notification.repository.contract;
+
+public class ExceptionConstants {
+
+    public static final String IMAGE_SERVICE_RESOURCE_NOT_AVAILABLE_OR_SERVICE_IS_DOWN =
+            "Image Service :: Resource not available or service is down";
+
+    private ExceptionConstants() {
+    }
+}
--- notification/src/main/java/com/social/notification/service/NotificationServiceImpl.java
+++ notification/src/main/java/com/social/notification/service/NotificationServiceImpl.java
@@ -4,14 +4,17 @@ import com.social.notification.model.Notification;
 import com.social.notification.repository.contract.NotificationRepository;
 import com.social.notification.service.contracts.NotificationService;
 import com.social.notification.service.feign.ImageClient;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
 import java.util.Set;
 
+import static com.social.notification.repository.contract.ExceptionConstants.IMAGE_SERVICE_RESOURCE_NOT_AVAILABLE_OR_SERVICE_IS_DOWN;
 import static com.social.notification.service.constants.LoggerConstants.*;
 import static com.social.notification.service.constants.ServiceConstants.*;
 
@@ -42,16 +45,21 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public List<Notification> findUserNotifications(String userIdentity) {
-        List<Notification> notifications = notificationRepository
-                .findUserNotifications(String.format(COLLECTION_TEMPLATE, userIdentity));
+        try {
+            List<Notification> notifications = notificationRepository
+                    .findUserNotifications(String.format(COLLECTION_TEMPLATE, userIdentity));
 
-        notifications.forEach(notification -> {
-            notification.setAuthorProfileImage(imageClient.findProfileImage(notification.getAuthorIdentity()));
-            notification.setPostedAgo(calculatePostedAgo(notification.getDate()));
-        });
+            notifications.forEach(notification -> {
+                notification.setAuthorProfileImage(imageClient.findProfileImage(notification.getAuthorIdentity()));
+                notification.setPostedAgo(calculatePostedAgo(notification.getDate()));
+            });
 
-        log.info(String.format(RETRIEVE_USER_NOTIFICATIONS_TEMPLATE, userIdentity));
-        return notifications;
+            log.info(String.format(RETRIEVE_USER_NOTIFICATIONS_TEMPLATE, userIdentity));
+            return notifications;
+        } catch (FeignException feignException) {
+            log.error(feignException.getMessage());
+            throw new ResourceAccessException(IMAGE_SERVICE_RESOURCE_NOT_AVAILABLE_OR_SERVICE_IS_DOWN);
+        }
     }
 
     private String calculatePostedAgo(LocalDate postDate) {
