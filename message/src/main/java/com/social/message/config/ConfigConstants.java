05703372e679e63074ab9cf942f1b09821ff9728
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-09 12:06:38 +0300

    In Message: Add Kafka consumer configurations

--- message/src/main/java/com/social/message/config/ConfigConstants.java
+++ message/src/main/java/com/social/message/config/ConfigConstants.java
@@ -11,4 +11,14 @@ public class ConfigConstants {
 
     public static final String MONGO_TEMPLATE_CREATED = MONGO_CONFIGURATION + "Mongo Template" + CREATED;
 
+    // Kafka Configuration Logger Constants
+    private static final String KAFKA_CONFIGURATION = "Kafka Configuration :: ";
+
+    private static final String FOR_KAFKA_MESSAGING_CREATED = " for Kafka messaging created";
+
+    public static final String DEFAULT_CONSUMER_FACTORY_FOR_KAFKA_MESSAGING_CREATED =
+            KAFKA_CONFIGURATION + "Default Consumer Factory" + FOR_KAFKA_MESSAGING_CREATED;
+
+    public static final String CONCURRENT_KAFKA_LISTENER_CONTAINER_FACTORY_FOR_KAFKA_MESSAGING_CREATED =
+            KAFKA_CONFIGURATION + "Concurrent Kafka Listener Container Factor" + FOR_KAFKA_MESSAGING_CREATED;
 }
