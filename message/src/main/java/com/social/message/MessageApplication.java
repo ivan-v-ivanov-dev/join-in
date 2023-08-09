e569656880175b652769ffe18d042de9dd040f0d
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-09 12:05:02 +0300

    In Message: Add MongoConfig; Disable AutoConfigurations

--- message/src/main/java/com/social/message/MessageApplication.java
+++ message/src/main/java/com/social/message/MessageApplication.java
@@ -2,8 +2,16 @@ package com.social.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        MongoAutoConfiguration.class})
 public class MessageApplication {
 
     public static void main(String[] args) {
---
+++ message/src/main/java/com/social/message/config/ConfigConstants.java
@@ -0,0 +1,14 @@
+package com.social.message.config;
+
+public class ConfigConstants {
+
+    // Mongo Configuration Logger Constants
+    private static final String MONGO_CONFIGURATION = "Mongo Configuration :: ";
+
+    private static final String CREATED = " created";
+
+    public static final String MONGO_CLIENT_CREATED = MONGO_CONFIGURATION + "Mongo Client" + CREATED;
+
+    public static final String MONGO_TEMPLATE_CREATED = MONGO_CONFIGURATION + "Mongo Template" + CREATED;
+
+}
