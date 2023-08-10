a401fa05640553714fcb2fe18614bda6b5f94a61
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-10 10:37:11 +0300

    In Message: Add RelationshipClient (Feign client)

--- message/src/main/java/com/social/message/MessageApplication.java
+++ message/src/main/java/com/social/message/MessageApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class,
         MongoAutoConfiguration.class})
+@EnableFeignClients
 public class MessageApplication {
 
     public static void main(String[] args) {
