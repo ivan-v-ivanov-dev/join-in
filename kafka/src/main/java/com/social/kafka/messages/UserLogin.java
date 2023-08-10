dba3736de6a712243985b03b1bdd9dfb601267b3
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-10 11:55:36 +0300

    In Kafka: Add UserLogin message

---
+++ kafka/src/main/java/com/social/kafka/messages/UserLogin.java
@@ -0,0 +1,12 @@
+package com.social.kafka.messages;
+
+import com.social.kafka.messages.contract.KafkaMessage;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public final class UserLogin implements KafkaMessage {
+
+    private final String identity;
+}
