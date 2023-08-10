f1984e04ef86a836508d8979d0ad66eae65f4e1c
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-10 10:24:55 +0300

    In Message: Add sample data for online users

---
+++ message/src/main/resources/mongo/init.js
@@ -0,0 +1,60 @@
+use ("admin");
+db.auth("rootuser", "rootpass");
+console.log('*** Authentication with root user ***')
+
+use ("storage");
+db.createCollection("online_users");
+console.log('*** Collection created ***')
+
+db.online_users.insertMany{
+    [
+        {
+            identity: "1567b3cb857f132473d4907fd3fee2f96e661308826522e823f5f2e6e20b6032",
+            online: true
+        },
+        {
+            identity: "771fda51cc819105d8f3ce802fce2a4f33d23054c5a102f19872fad2a3208551",
+            online: true
+        },
+        {
+            identity: "788176f5bb7f654863c2f3aa6bfaa8b49f54d0288019e9d846f772467984b627",
+            online: true
+        },
+        {
+            identity: "d52ae056d5830fe0a58a7da85465d6561bb0b9aba01706f010c585810b8fd3c8",
+            online: true
+        },
+        {
+            identity: "8a27182c66548a4dcdc9b0cbf26227defcc076a464faa6b66e9f1c876590c94f",
+            online: true
+        },
+        {
+           identity: "c5f825c26e2edad6bedd8c23b5da3006638793c4a38faaa6bd49cfbe4ab6dd85",
+           online: true
+        },
+        {
+           identity: "519ba91a5a5b4afb9dc66f8805ce8c442b6576316c19c6896af2fa9bda6aff71",
+           online: true
+        },
+        {
+           identity: "a4fe133a6f4bc82ab1e102ba5bdc6aaa66b3e69bbfd6e64ac30ebf7708e8bff6",
+           online: true
+        },
+        {
+           identity: "31cae906048ac3f9dcd9525d0db54d766ffeca05e3c6f87e92f96306b8122d93",
+           online: true
+        },
+        {
+           identity: "67c80c58573c12562067629782b72c455fbd8ab06bbf8dfbd4bc20331d1cbedf",
+           online: true
+        },
+        {
+           identity: "41d52bee906b4347466558329fb7a6cbc24b5ab0a5f6c58e2e614decc764fab3",
+           online: true
+        },
+        {
+           identity: "cda07e665379ec023b0577605bfd6f91770fe997d72b4e7d3b90e17c29a1ad9e",
+           online: true
+        }
+    ]
+}
