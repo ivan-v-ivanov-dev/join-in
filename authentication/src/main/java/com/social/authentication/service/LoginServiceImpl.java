0d5b13e73432c09535466b5a05258a4b7a7fd377
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-10 11:38:03 +0300

    In Authentication: Send Kafka Message for the new user who logged in

--- authentication/src/main/java/com/social/authentication/service/LoginServiceImpl.java
+++ authentication/src/main/java/com/social/authentication/service/LoginServiceImpl.java
@@ -4,9 +4,13 @@ import com.social.authentication.model.Log;
 import com.social.authentication.model.User;
 import com.social.authentication.repository.LogRepository;
 import com.social.authentication.repository.UserRepository;
+import com.social.authentication.service.contract.KafkaMessageSender;
 import com.social.authentication.service.contract.LoginService;
 import com.social.authentication.service.contract.PasswordEncoder;
+import com.social.kafka.messages.UserLogin;
+import com.social.kafka.messages.contract.KafkaMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -22,13 +26,19 @@ public class LoginServiceImpl implements LoginService {
     private final LogRepository logRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final KafkaMessageSender kafkaMessageSender;
+    private String userLoginTopic;
 
     public LoginServiceImpl(LogRepository logRepository,
                             UserRepository userRepository,
-                            PasswordEncoder passwordEncoder) {
+                            PasswordEncoder passwordEncoder,
+                            KafkaMessageSender kafkaMessageSender,
+                            @Value("${spring.kafka.topic.name.user.login}") String userLoginTopic) {
         this.logRepository = logRepository;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.kafkaMessageSender = kafkaMessageSender;
+        this.userLoginTopic = userLoginTopic;
     }
 
     @Override
@@ -45,6 +55,9 @@ public class LoginServiceImpl implements LoginService {
         Log loggedUserLog = Log.builder().id(UUID.randomUUID().toString()).user(loggedUser).logDate(LocalDate.now()).build();
         logRepository.save(loggedUserLog);
         log.info(String.format(USER_WITH_IDENTITY_LOGIN_SAVED_IN_DATABASE_TEMPLATE, loggedUser.getIdentity()));
+
+        KafkaMessage userLogin = UserLogin.builder().identity(loggedUser.getIdentity()).build();
+        kafkaMessageSender.send(userLogin, userLoginTopic);
 
         return loggedUser.getIdentity();
     }
