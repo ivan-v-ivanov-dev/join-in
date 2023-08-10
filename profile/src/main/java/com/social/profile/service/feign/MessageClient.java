0a2e232dbee4a9b69566ebd3e0c676d581b979ba
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-10 15:29:08 +0300

    In Profile: Add MessageClient

---
+++ profile/src/main/java/com/social/profile/service/feign/MessageClient.java
@@ -0,0 +1,15 @@
+package com.social.profile.service.feign;
+
+import com.social.profile.model.OnlineFriend;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Set;
+
+@FeignClient(name = "${message.service.feign.client.name}", url = "${message.service.url}")
+public interface MessageClient {
+
+    @PostMapping("${friends.online}")
+    Set<OnlineFriend> findUserOnlineFriends(@RequestParam("identity") String identity);
+}
