55d1bc9a33f863c41d2b879e7bd689b1b1e0dd49
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-14 18:51:02 +0300

    In Profile: Add PostMapping("${direct.chat.history}") to MessageClient

--- profile/src/main/java/com/social/profile/service/feign/MessageClient.java
+++ profile/src/main/java/com/social/profile/service/feign/MessageClient.java
@@ -1,10 +1,12 @@
 package com.social.profile.service.feign;
 
+import com.social.profile.model.DirectChat;
 import com.social.profile.model.OnlineFriend;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Set;
 
 @FeignClient(name = "${message.service.feign.client.name}", url = "${message.service.url}")
@@ -12,4 +14,7 @@ public interface MessageClient {
 
     @PostMapping("${friends.online}")
     Set<OnlineFriend> findUserOnlineFriends(@RequestParam("identity") String identity);
+
+    @PostMapping("${direct.chat.history}")
+    List<DirectChat> findUserDirectChatHistory(@RequestParam("identity") String identity);
 }
