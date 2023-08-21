3e64e10516eb2ee0ef2ba0106626b25934cacbaf
Ivan Ivanov <ivanov.biol@gmail.com>
2023-08-21 10:47:14 +0300

    In Kafka: Add NewChatMessage

---
+++ kafka/src/main/java/com/social/kafka/messages/NewChatMessage.java
@@ -0,0 +1,20 @@
+package com.social.kafka.messages;
+
+import com.social.kafka.messages.contract.KafkaMessage;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Getter
+@Builder(toBuilder = true)
+public final class NewChatMessage implements KafkaMessage {
+
+    private final String messageContent;
+
+    private final String chatIdentity;
+
+    private final String senderIdentity;
+}
