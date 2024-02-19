9a22cf225ce548ab858c8a2ab02ca781637dd16a
Ivan Ivanov <ivanov.biol@gmail.com>
2024-02-19 20:16:59 +0200

    initial project build

---
+++ src/main/java/com/social/joinin/JoinInApplication.java
@@ -0,0 +1,13 @@
+package com.social.joinin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/joinin/JoinInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.joinin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
