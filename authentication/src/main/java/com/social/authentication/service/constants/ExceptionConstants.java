f7b1b6ab12096356d922924733821dc34a9b5c36
Ivan Ivanov <ivan.v.ivanovdev@gmail.com>
2024-08-07 18:41:24 +0300

    In Authentication: Add constant

--- authentication/src/main/java/com/social/authentication/service/constants/ExceptionConstants.java
+++ authentication/src/main/java/com/social/authentication/service/constants/ExceptionConstants.java
@@ -4,6 +4,11 @@ public class ExceptionConstants {
 
     public static final String BAD_CREDENTIALS = "Bad credentials";
 
+    public static final String USER_ALREADY_EXISTS = "User already exists";
+
+    public static final String USER_ALREADY_EXISTS_WITH_EMAIL_TEMPLATE =
+            USER_ALREADY_EXISTS + " with email: %s";
+
     private ExceptionConstants() {
     }
 }
