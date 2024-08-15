7ff4c93f4b2b1211428b07896d4db2ae5c04253d
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-08-15 16:20:37 +0300

    Create Image service

---
+++ image/src/main/java/com/social/image/controller/ImageController.java
@@ -0,0 +1,13 @@
+package com.social.image.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Image service is HEALTHY";
+    }
+}
