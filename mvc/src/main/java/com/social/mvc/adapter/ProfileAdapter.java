1010ae2b7de2b4401cded0af7508f68d803faed0
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-11-11 16:57:24 +0200

    In Gateway: Fix mapper

--- mvc/src/main/java/com/social/mvc/adapter/ProfileAdapter.java
+++ mvc/src/main/java/com/social/mvc/adapter/ProfileAdapter.java
@@ -4,7 +4,7 @@ import com.social.model.dto.ProfileRp;
 import com.social.mvc.model.Profile;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ProfileAdapter {
 
     Profile fromProfileRpToProfile(ProfileRp byIdentity);
