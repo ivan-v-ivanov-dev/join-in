940084cd7a31296c2e8eb4f25a937d8e5a906d7c
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-11-11 16:58:22 +0200

    In Notification: Use lombok

--- notification/src/main/java/com/social/notification/service/NotificationListener.java
+++ notification/src/main/java/com/social/notification/service/NotificationListener.java
@@ -4,6 +4,7 @@ import com.social.kafka.messages.RegisteredUserMessage;
 import com.social.kafka.messages.contract.KafkaMessage;
 import com.social.notification.service.contract.NotificationService;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,6 +14,7 @@ import static com.social.notification.service.constants.LoggerConstants.NEW_REGI
 
 @Service
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 @Slf4j
 public class NotificationListener {
 
