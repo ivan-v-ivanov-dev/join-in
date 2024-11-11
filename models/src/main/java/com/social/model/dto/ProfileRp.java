1cfa16d0a7dbe87cd36874397b293eaef5a3fbe5
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-11-11 16:58:51 +0200

    In Models: Use lombok

--- models/src/main/java/com/social/model/dto/ProfileRp.java
+++ models/src/main/java/com/social/model/dto/ProfileRp.java
@@ -3,12 +3,14 @@ package com.social.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 public class ProfileRp {
