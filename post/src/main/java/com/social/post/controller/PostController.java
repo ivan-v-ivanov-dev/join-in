a4b1c9ef6c292456078f9763f2f0006bf63cd080
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-11-12 16:46:08 +0200

    In Post: GetMapping("/author/{authorIdentity}/post/{postIdentity}") returns ResponseEntity

--- post/src/main/java/com/social/post/controller/PostController.java
+++ post/src/main/java/com/social/post/controller/PostController.java
@@ -3,6 +3,7 @@ package com.social.post.controller;
 import com.social.post.model.Post;
 import com.social.post.service.contract.PostService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,11 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/author/{authorIdentity}/post/{postIdentity}")
-    public Post findByPostIdentity(@PathVariable("authorIdentity") String authorIdentity,
-                                   @PathVariable("postIdentity") String postIdentity) {
-        return postService.findByAuthorIdentityAndPostIdentity(authorIdentity, postIdentity);
+    public ResponseEntity<Post> findByPostIdentity(@PathVariable("authorIdentity") String authorIdentity,
+                                                   @PathVariable("postIdentity") String postIdentity) {
+        return ResponseEntity.ok(postService.findByAuthorIdentityAndPostIdentity(authorIdentity, postIdentity));
     }
+
     @GetMapping("/health")
     public String health() {
         return "Post service is HEALTHY";
