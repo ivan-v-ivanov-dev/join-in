6e5e2d36ada56ec7b2ba5627f05c6acde4a51f52
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-11-12 11:07:18 +0200

    In Gateway: Change POST to GET a profile

--- gateway/src/main/java/com/social/gateway/service/feign/ProfileClient.java
+++ gateway/src/main/java/com/social/gateway/service/feign/ProfileClient.java
@@ -2,12 +2,12 @@ package com.social.gateway.service.feign;
 
 import com.social.model.dto.ProfileRp;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "${profile.service.feign.client.name}", url = "${profile.service.url}")
 public interface ProfileClient {
 
-    @PostMapping("${profile.identity.endpoint}")
+    @GetMapping("${profile.identity.endpoint}")
     ProfileRp findByIdentity(@PathVariable("identity") String identity);
 }
