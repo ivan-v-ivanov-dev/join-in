c8e7f7d68ec71fdeb6f818e7479a38161d609285
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-11-12 11:07:10 +0200

    In Profile: Change POST to GET a profile

--- profile/src/main/java/com/social/profile/controller/ProfileController.java
+++ profile/src/main/java/com/social/profile/controller/ProfileController.java
@@ -5,7 +5,6 @@ import com.social.profile.service.contract.ProfileService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +13,7 @@ public class ProfileController {
 
     private final ProfileService profileService;
 
-    @PostMapping("/profile/{identity}")
+    @GetMapping("/profile/{identity}")
     public ProfileRp findByIdentity(@PathVariable("identity") String identity) {
         return profileService.findByIdentity(identity);
     }
