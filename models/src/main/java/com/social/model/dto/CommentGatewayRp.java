c0d0107e7b52b9b4942c8dd4b8d7a50317e1c458
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-11-13 16:21:36 +0200

    In Model: Add setter in CommentGatewayRp

--- models/src/main/java/com/social/model/dto/CommentGatewayRp.java
+++ models/src/main/java/com/social/model/dto/CommentGatewayRp.java
@@ -3,10 +3,12 @@ package com.social.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Builder
 @Getter
+@Setter
 public class CommentGatewayRp {
 
     private String commentIdentity;
