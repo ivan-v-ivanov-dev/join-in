d37ddcb2564f392ff3bf9b963d69683687da18c6
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-11-14 13:36:08 +0200

    In Reaction: Code refactor

--- reaction/src/main/java/com/social/reaction/service/ReactionListener.java
+++ reaction/src/main/java/com/social/reaction/service/ReactionListener.java
@@ -4,6 +4,7 @@ import com.social.kafka.messages.RegisteredUserMessage;
 import com.social.kafka.messages.contract.KafkaMessage;
 import com.social.reaction.service.contract.ProfileService;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,6 +14,7 @@ import static com.social.reaction.service.constants.LoggerConstants.NEW_REGISTER
 
 @Service
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 @Slf4j
 public class ReactionListener {
 
