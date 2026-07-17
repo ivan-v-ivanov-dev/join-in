485d20547f7cc88d0a47b7087c0d88d84ae2e324
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-17 15:18:03 +0300

    change the group-d for kafka and leave only the consumer

--- profile/src/main/java/com/joinin/profile/configuration/KafkaConfig.java
+++ profile/src/main/java/com/joinin/profile/configuration/KafkaConfig.java
@@ -1,19 +1,16 @@
 package com.joinin.profile.configuration;
 
 import com.join_in.kafka_models.KafkaMessage;
-import com.join_in.kafka_models.messages.RegisterNewUser;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
-import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,22 +26,6 @@ public class KafkaConfig {
     private String trustedPackages;
     @Value("${spring.kafka.offset-reset}")
     private String offsetReset;
-
-    @Bean
-    public ProducerFactory<String, KafkaMessage> producerFactory() {
-
-        Map<String, Object> properties = new HashMap<>();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(properties);
-    }
-
-    @Bean
-    public KafkaTemplate<String, KafkaMessage> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
 
     @Bean
     public ConsumerFactory<String, KafkaMessage> consumerFactory() {
