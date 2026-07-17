5528819a267c7db3bd7b293c142d5e8ba6c60828
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-17 09:35:18 +0300

    Rename RegisterNewUserTopic

--- gateway/src/main/java/com/joinin/gateway/configuration/RegisterNewUserTopic.java
+++ gateway/src/main/java/com/joinin/gateway/configuration/RegisterNewUserTopic.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-public class KafkaTopicConfiguration {
+public class RegisterNewUserTopic {
 
     @Value("${spring.kafka.topic.register-new-user}")
     private String registerNewUserTopic;
