21847c124d5d965c059179d96a82b00630907cc1
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-21 14:41:54 +0300

    Rename ProfileRepository

--- reaction/src/main/java/com/joinin/reaction/repository/ProfileRepository.java
+++ reaction/src/main/java/com/joinin/reaction/repository/ProfileRepository.java
@@ -3,5 +3,5 @@ package com.joinin.reaction.repository;
 import com.joinin.reaction.model.ProfileNode;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
-public interface ProfileNodeRepository extends Neo4jRepository<ProfileNode, String> {
+public interface ProfileRepository extends Neo4jRepository<ProfileNode, String> {
 }
--- reaction/src/main/java/com/joinin/reaction/service/ProfileServiceImpl.java
+++ reaction/src/main/java/com/joinin/reaction/service/ProfileServiceImpl.java
@@ -1,7 +1,7 @@
 package com.joinin.reaction.service;
 
 import com.joinin.reaction.model.ProfileNode;
-import com.joinin.reaction.repository.ProfileNodeRepository;
+import com.joinin.reaction.repository.ProfileRepository;
 import com.joinin.reaction.service.contract.ProfileService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,14 +12,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ProfileServiceImpl implements ProfileService {
 
-    private final ProfileNodeRepository profileNodeRepository;
+    private final ProfileRepository profileRepository;
 
     @Override
     public void saveProfile(String identity) {
         ProfileNode user = ProfileNode.builder()
                 .identity(identity)
                 .build();
-        ProfileNode savedProfileNode = profileNodeRepository.save(user);
+        ProfileNode savedProfileNode = profileRepository.save(user);
         log.info("User node saved in database. User identity: " + savedProfileNode.getIdentity());
     }
 }
