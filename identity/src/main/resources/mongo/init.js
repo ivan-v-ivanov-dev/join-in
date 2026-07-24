eaa58280e60f11e7a0047933b9a3fafebb9c9de9
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-24 14:39:43 +0300

    Comments for more details

--- identity/src/main/resources/mongo/init.js
+++ identity/src/main/resources/mongo/init.js
@@ -7,11 +7,13 @@ db.createCollection("profiles");
 console.log('*** Collection created ***')
 
 db.profiles.insertMany([
-    {
+ {
     identity: "429e71a7f1773bf2619c4c54ca4a7c67ab949efc7fffc1097a9629e8dff3528b",
     email: "ivan@ivan.com",
     password: "$2b$12$LznTPqlU2E02QQoNGPZOwuNTRgZu.R2z/8rx8tM1lQHBR8k.p24vC"
   },
+
+   //FRIENDS
   {
     identity: "9b5d4f5c0a71d7fef2c7c9f5f8cb45f4c1d49d6c9d4c9b9e3d6f2b1c4f8e7a91",
     email: "petur@petur.com",
@@ -57,21 +59,26 @@ db.profiles.insertMany([
     email: "tanya@tanya.com",
     password: "$2b$10$IvL6PqRsTuVwXyZaBcDeFgHiJkLmNoPqRsTuVwXyZaB987654321"
   },
+
+    //MOTHER
   {
     identity: "67c80c58573c12562067629782b72c455fbd8ab06bbf8dfbd4bc20331d1cbedf",
     email: "silviya@silviya.com",
     password: "$2b$10$JwM5XyZaBcDeFgHiJkLmNoPqRsTuVwXyZaBcDeFgHi123456789"
   },
+  //FATHER
   {
     identity: "41d52bee906b4347466558329fb7a6cbc24b5ab0a5f6c58e2e614decc764fab3",
     email: "teodor@teodor.com",
     password: "$2b$10$KxN3FgHiJkLmNoPqRsTuVwXyZaBcDeFgHiJkLmNoPqR987654321"
   },
+  //BROTHER
   {
     identity: "cda07e665379ec023b0577605bfd6f91770fe997d72b4e7d3b90e17c29a1ad9e",
     email: "asen@asen.com",
     password: "$2b$10$LyP7JkLmNoPqRsTuVwXyZaBcDeFgHiJkLmNoPqRsTuV123456789"
   },
+  // NON FRIENDS
   {
     identity: "8b94a3f1bdb9d79a74bb71d933c6d0dd6c4dd08f75b89d1cbfcdf462cb0b7fd2",
     email: "martin@martin.com",
@@ -86,6 +93,23 @@ db.profiles.insertMany([
     identity: "3d9abcb92d1d574ef0d0d49b0cb62cb6d6fd6bb49d82f8a3a8e4a7a0d3aefb11",
     email: "nikol@nikol.com",
     password: "$2b$10$ObS2TuVwXyZaBcDeFgHiJkLmNoPqRsTuVwXyZaBcDeF987654321"
+  },
+
+  //FRIENDSHIP REQUEST
+  {
+    identity: "f1bc296565b36ec1932fd0b0889fb94f3fc14f56f1141f1327fd4f58cfa6a231",
+    email: "aleksandar@aleksandar.com",
+    password: "$2a$10$gBJxHiRnZvk1u7f6ttVIuOgMEmg7A3fLhfNuym46Hk.1vWZBkV8.S"
+  },
+  {
+    identity: "991f49289c5745bbeba353af157a40c48edfed061ead6d417160d9b89a54baa6",
+    email: "desislava@desislava.com",
+    password: "$2a$10$KdKT4VEYTIYAGlEt8a7bcOUw9DhY7aqdXj9wonIuD0NajVn8dw1uy"
+  },
+  {
+    identity: "aad12f3aa20775b329a00d0f0ccba97dafbc72fd925163e2e26f5ee91ec6b2cd",
+    email: "borislav@borislav.com",
+    password: "$2a$10$EW2EEo1uTnU3NwKSp.OmTOxYQLWOOR0DKto3RpwdLYVZVEnjal0qq"
   }
 ]);
 
