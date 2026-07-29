acf3a9f9093266646a57417bd9babea0f4483b31
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-29 17:02:39 +0300

    Add MediaServiceClient

--- post/src/main/java/com/joinin/post/mapper/CommentResponseMapper.java
+++ post/src/main/java/com/joinin/post/mapper/CommentResponseMapper.java
@@ -1,11 +1,14 @@
 package com.joinin.post.mapper;
 
 import com.join_in.common_models.CommentRpPostService;
+import com.join_in.common_models.ProfileImageRpMediaService;
 import com.joinin.post.model.CommentEntity;
 import com.joinin.post.model.CommentPrimaryKey;
-import com.joinin.post.model.CommentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +16,19 @@ public class CommentResponseMapper {
 
     private final PostedAgoFormatter postedAgoFormatter;
 
-    public CommentRpPostService fromCommentEntitytoCommentRpPostService(CommentEntity commentEntity) {
+    public CommentRpPostService fromCommentEntitytoCommentRpPostService(CommentEntity commentEntity, List<ProfileImageRpMediaService> profileImagesRpMediaServices) {
         CommentPrimaryKey primaryKey = commentEntity.getPrimaryKey();
 
+        String image = profileImagesRpMediaServices
+                .stream()
+                .filter(e -> Objects.equals(e.identity(), commentEntity.getAuthorIdentity()))
+                .findFirst().get()
+                .profileImage();
+
         return new CommentRpPostService(
+                image,
+                //TODO Add profile name
+                "Ivan",
                 primaryKey.getCommentIdentity(),
                 primaryKey.getPostIdentity(),
                 commentEntity.getAuthorIdentity(),
--- post/src/main/java/com/joinin/post/service/PostServiceImpl.java
+++ post/src/main/java/com/joinin/post/service/PostServiceImpl.java
@@ -2,14 +2,15 @@ package com.joinin.post.service;
 
 import com.join_in.common_models.CommentRpPostService;
 import com.join_in.common_models.PostRpPostService;
+import com.join_in.common_models.ProfileImageRpMediaService;
 import com.joinin.post.mapper.CommentResponseMapper;
 import com.joinin.post.mapper.PostResponseMapper;
-import com.joinin.post.model.CommentResponse;
+import com.joinin.post.model.CommentEntity;
 import com.joinin.post.model.PostByAuthorEntity;
-import com.joinin.post.model.PostResponse;
 import com.joinin.post.repository.CommentRepository;
 import com.joinin.post.repository.PostByAuthorRepository;
 import com.joinin.post.service.contract.PostService;
+import com.joinin.post.service.feign.MediaServiceClient;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,7 @@ public class PostServiceImpl implements PostService {
     private final CommentRepository commentRepository;
     private final PostResponseMapper postResponseMapper;
     private final CommentResponseMapper commentResponseMapper;
+    private final MediaServiceClient mediaServiceClient;
 
     @Override
     public List<PostRpPostService> retrievePostsByAuthor(String identity) {
@@ -33,16 +35,15 @@ public class PostServiceImpl implements PostService {
 
         List<PostRpPostService> posts = postEntities.stream()
                 .map(postEntity -> {
-
                     String postIdentity = postEntity.getKey().getPostIdentity();
-
-                    List<CommentRpPostService> comments = commentRepository
-                            .findByPostIdentity(postIdentity)
-                            .stream()
-                            .map(commentResponseMapper::fromCommentEntitytoCommentRpPostService)
+                    List<CommentEntity> commentEntities = commentRepository.findByPostIdentity(postIdentity);
+                    List<String> commentAuthorIdentities = commentEntities.stream().map(CommentEntity::getAuthorIdentity).toList();
+                    List<ProfileImageRpMediaService> profileImagesRpMediaServices = mediaServiceClient.retrieveProfileImagesForProfiles(commentAuthorIdentities);
+                    //TODO Call profile service to retrieve user names
+                    List<CommentRpPostService> commentRpPostServices = commentEntities.stream()
+                            .map(e -> commentResponseMapper.fromCommentEntitytoCommentRpPostService(e, profileImagesRpMediaServices))
                             .toList();
-
-                    return postResponseMapper.fromPostByAuthortoPostRpPostService(postEntity, comments);
+                    return postResponseMapper.fromPostByAuthortoPostRpPostService(postEntity, commentRpPostServices);
                 })
                 .toList();
         log.info("Retrieve comments for all posts");
